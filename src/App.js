1c8032a6b9bd61c6b917f4c5fa91aef099e0bf3a
gonsakon <gonsakon@gmail.com>
2022-08-17 21:21:59 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import FAQ from './components/FAQ';
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/faq">FAQ</Link>
+      </nav>
+    </>
+  );
+}
 
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+function NotFound(){
+  return(
+    <>
+      <h2>您的網址出錯嘍~</h2>
+      <Link to="/">回到首頁</Link>
+    </>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Welcome to React Router!</h1>
+       {/* 註冊路由表 */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="faq" element={<FAQ />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/FAQ.js
@@ -0,0 +1,11 @@
+import {Link}  from "react-router-dom";
+function FAQ(){
+    return(
+      <>
+        <h2>FAQ</h2>
+        <Link to="/">home</Link>
+      </>
+    )
+}
+
+export default FAQ;
